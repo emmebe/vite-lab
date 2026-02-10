c86c34eed230abb1abf167d8319a63c0d388195d
Emma <ebelinge@msu.edu>
2026-02-10 16:33:40 +0000

    changed logo svgs and subheaders

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { FaReact, FaRocket, FaHeart, FaCode } from 'react-icons/fa'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -10,10 +11,10 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <FaHeart className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <FaRocket className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Emma Learns Vite + React</h1>
@@ -22,11 +23,11 @@ function App() {
           You've clicked {count} times
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          I edited the header, subheaders, and svgs.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the pictures to see more of me. 
+        I learned how to edit Vite + React 
       </p>
     </>
   )
